d526e785b3df302ccb9ec4f2eb582e0eaab5c331
jona881a <jonaskunert@hotmail.com>
2022-02-18 09:46:38 +0100

    make it better

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ app.get("/", (req, res) => {
   res.sendFile(`${__dirname}/public/index.html`);
 });
 
-const PORT = 8080;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log("Server is running with port", PORT);
 });
